2e88d3c62a80bde8d921e35709174e1808829be1
Phoenix-1984 <victode26@gmail.com>
2026-02-25 12:35:35 -0500

    Concluido mi tercer miniproyecto

---
+++ 02-Mini-proyecto.py
@@ -0,0 +1,57 @@
+'''
+MINI PROYECTO: "EVALUADOR DE ESTUDIANTE"
+Crear un programa que pida:
+1. Nombre y edad del estudiante
+2. Sus 3 notas
+3. Calcular el promedio 
+4. Determinar si: 
+- Está aprobado (promedio >= 11)
+- Tiene excelencia (promedio >= 18)
+- Está desaprobado
+5.Determinar si puede acceder a una beca:
+-Promedio mayor o igual a 16
+-Edad menos o igual a 20
+'''
+
+#Información del alumno
+nombre = input('Ingresa tu nombre :')
+edad = input('Ingresa tu edad :')
+nota_1 = input('Ingresa nota 1 :')
+nota_2 = input('Ingresa nota 2 :')
+nota_3 = input('Ingresa nota 3 :')
+
+#Convertimos tipos
+edad = int(edad)
+nota_1 = float(nota_1)
+nota_2 = float(nota_2)
+nota_3 = float(nota_3)
+
+#Calculos
+prom = (nota_1 + nota_2 + nota_3)/3
+
+#Determinar Estado: 
+if  prom >= 18:
+    estado = ("Excelente")
+
+if prom >= 11 and prom < 18:
+    estado = ("Aprobado")
+
+if prom < 11:
+    estado = ("Desaprobado")
+
+
+#Puede acceder a una beca:
+beca = "No apto"
+
+if prom >= 16 and edad <= 20:
+    beca = ("Apto")
+
+#Imprimir 
+print("Estudiante", nombre)
+print("Edad", edad)
+print("Nota 1", nota_1)
+print("Nota 2", nota_2)
+print("Nota 3", nota_3)
+print("Promedio", prom)
+print("Estado", estado)
+print("¿Accede a beca?: ", beca)
---
+++ 02-Operator.py
@@ -0,0 +1,51 @@
+### Operadores Aritméticos ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)
+print(10 // 3)
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+print("Hola " + "Python " + " ¿Qué tal?")
+print("Hola " + str(5))
+print("Hola " * 5)
+print("Hola " * (2 ** 3))
+
+my_float = 2.5 * 2
+print("Hola" * int(my_float))
+
+### Operadores Comparativos ###
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+#Ordenación alfabética
+# Python compara texto letra por letra usando el valor Unicode
+# Es decir, realmente compara números internos de cada carácter
+print("Hola" >  "Python") # Compara la primera letra: "H"(72)>"P" (80) - False
+print("Hola" <  "Python")
+print("Hola" >= "Python")
+print("Hola" <= "Python")
+print("Hola" == "Python")
+print("Hola" != "Python")
+
+### Operadores Lógicos ###
+print(3 > 4 and "Hola" > "Python")
+#print(3 > 4 or "Hola" > "Python")
+#print(3 > 4 not "Hola" > "Python")
+
+
+
+
+
+
+
+
+
+
